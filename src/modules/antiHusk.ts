a1886f1a9b541e4622acdd71a7f1c56ac30ac962
Vendicated <vendicated@riseup.net>
2024-05-13 04:07:13 +0200

    add anti husk

---
+++ src/modules/antiHusk.ts
@@ -0,0 +1,15 @@
+import { Vaius } from "../Client";
+
+const HuskAbuserIds = new Set([
+    "886685857560539176", // nino
+    "259558259491340288", // sqakoi
+]);
+
+Vaius.on("messageReactionAdd", async (msg, reactor, reaction) => {
+    if (!msg.guildID) return;
+    if (reaction.name !== "husk") return;
+
+    if (HuskAbuserIds.has(reactor.id)) {
+        await Vaius.rest.channels.deleteReaction(msg.channelID, msg.id, `husk:${reaction.id}`, reactor.id);
+    }
+});
