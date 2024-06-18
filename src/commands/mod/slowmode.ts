98e90d4877c7fa7556e07e691f6496a72f60de57
Vendicated <vendicated@riseup.net>
2024-06-18 22:42:21 +0200

    add slowmode command

---
+++ src/commands/mod/slowmode.ts
@@ -0,0 +1,24 @@
+import { defineCommand } from "~/Command";
+import { Emoji } from "~/constants";
+import { reply, silently } from "~/util";
+
+defineCommand({
+    name: "slowmode",
+    aliases: ["slow"],
+    description: "Set the slowmode for the channel",
+    permissions: ["MANAGE_CHANNELS"],
+    usage: "<seconds>",
+    guildOnly: true,
+    async execute(msg, secondsArg) {
+        if (secondsArg === "off") secondsArg = "0";
+
+        const seconds = Number(secondsArg);
+
+        if (isNaN(seconds) || seconds < 0 || seconds > 21600)
+            return reply(msg, "SlowMode must be between 0 and 21600 seconds!");
+
+        await msg.channel.edit({ rateLimitPerUser: seconds });
+
+        silently(msg.createReaction(Emoji.CheckMark));
+    }
+});
