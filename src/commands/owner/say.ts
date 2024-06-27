b030f9e4705428f2afc7c16d91ea84b24213e00c
Vendicated <vendicated@riseup.net>
2024-06-27 10:40:01 +0200

    say: add user install option

--- src/commands/owner/say.ts
+++ src/commands/owner/say.ts
@@ -1,4 +1,4 @@
-import { ApplicationCommandOptionTypes, ApplicationCommandTypes, InteractionTypes, MessageFlags } from "oceanic.js";
+import { ApplicationCommandOptionTypes, ApplicationCommandTypes, ApplicationIntegrationTypes, InteractionTypes, MessageFlags } from "oceanic.js";
 
 import { OwnerId, Vaius } from "~/Client";
 
@@ -23,19 +23,40 @@ Vaius.once("ready", () => {
             }
         ]
     });
+
+    Vaius.application.createGlobalCommand({
+        integrationTypes: [ApplicationIntegrationTypes.USER_INSTALL],
+        type: ApplicationCommandTypes.CHAT_INPUT,
+        name: "say",
+        description: "say",
+        defaultMemberPermissions: "0",
+        options: [
+            {
+                name: "content",
+                description: "content",
+                type: ApplicationCommandOptionTypes.STRING,
+                required: true
+            },
+            {
+                name: "reply-to",
+                description: "reply",
+                type: ApplicationCommandOptionTypes.STRING,
+                required: false
+            }
+        ]
+    });
 });
 
 Vaius.on("interactionCreate", async i => {
     if (i.user.id !== OwnerId) return;
     if (i.type !== InteractionTypes.APPLICATION_COMMAND || i.data.name !== "say") return;
-    if (!i.inCachedGuildChannel()) return;
 
     const content = i.data.options.getString("content", true);
     const reply = i.data.options.getString("reply-to");
 
     await i.defer(MessageFlags.EPHEMERAL);
 
-    await i.channel.createMessage({
+    await Vaius.rest.channels.createMessage(i.channelID, {
         content,
         messageReference: reply ? {
             messageID: reply
@@ -47,5 +68,6 @@ Vaius.on("interactionCreate", async i => {
             users: true
         }
     });
+
     await i.createFollowup({ content: "done", flags: MessageFlags.EPHEMERAL });
 });
