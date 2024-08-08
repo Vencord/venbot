9855567adb5b395037e48c61a8d78c95ffc10321
Vendicated <vendicated@riseup.net>
2024-08-08 22:03:45 +0200

    add stickerleaderboard

--- src/commands/expression-tracker/commands.ts
+++ src/commands/expression-tracker/commands.ts
@@ -125,9 +125,20 @@ defineCommand({
     },
 });
 
-const makeLeaderboard = (usageType: ExpressionUsageType) => async (msg: Message, emoji: string) => {
-    const id = customEmojiRe.exec(emoji)?.[1] ?? emoji;
-    customEmojiRe.lastIndex = 0;
+const makeLeaderboard = (usageType: ExpressionUsageType, expressionType = ExpressionType.EMOJI) => async (msg: Message, emoji: string) => {
+    let name = emoji;
+    let id: string | undefined;
+    if (expressionType === ExpressionType.EMOJI) {
+        id = customEmojiRe.exec(emoji)?.[1] ?? emoji;
+        customEmojiRe.lastIndex = 0;
+    } else {
+        const sticker = msg.stickerItems?.[0];
+        if (sticker) {
+            ({ name, id } = sticker);
+        }
+    }
+
+    if (!id) return reply(msg, { content: `Did you forget to specify the ${expressionType}?` });
 
     const stats = await db.selectFrom("expressionUses")
         .select(({ fn }) => [
@@ -137,17 +148,18 @@ const makeLeaderboard = (usageType: ExpressionUsageType) => async (msg: Message,
         ])
         .where(eb => eb.and({
             id,
-            usageType
+            usageType,
+            expressionType
         }))
         .groupBy("userId")
         .orderBy("count", "desc")
         .execute();
 
     if (!stats.length)
-        return reply(msg, `Either no one has used ${toInlineCode(emoji)} yet, or it's not a valid emoji!`);
+        return reply(msg, `Either no one has used ${toInlineCode(emoji)} yet, or it's not a valid ${expressionType}!`);
 
     const paginator = new Paginator(
-        toTitle(`Top  ${emoji}  ${usageType === ExpressionUsageType.MESSAGE ? "Users" : "Reactors"}`),
+        toTitle(`Top  ${name}  ${usageType === ExpressionUsageType.MESSAGE ? "Users" : "Reactors"}`),
         stats,
         20,
         users => formatCountAndName(users.map(({ count, userId }) => [count.toString(), `<@${userId}>`])),
@@ -160,7 +172,7 @@ const makeLeaderboard = (usageType: ExpressionUsageType) => async (msg: Message,
 defineCommand({
     name: "emoji-leaderboard",
     aliases: ["elb", "e-lb", "emojilb", "emoji-lb"],
-    description: "Check who used an emoji the most",
+    description: "Check who sent an emoji the most",
     usage: "<emoji>",
     execute: makeLeaderboard(ExpressionUsageType.MESSAGE),
 });
@@ -171,4 +183,12 @@ defineCommand({
     description: "Check who reacted with an emoji the most",
     usage: "<emoji>",
     execute: makeLeaderboard(ExpressionUsageType.REACTION),
+});
+
+defineCommand({
+    name: "sticker-leaderboard",
+    aliases: ["slb", "s-lb", "stickerlb", "sticker-lb"],
+    description: "Check who sent a sticker the most",
+    usage: "<sticker>",
+    execute: makeLeaderboard(ExpressionUsageType.MESSAGE, ExpressionType.STICKER),
 });
