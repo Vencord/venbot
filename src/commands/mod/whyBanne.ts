81281dea6af15f81f4f17d7a84bad738c3d82402
Vendicated <vendicated@riseup.net>
2024-08-09 20:08:28 +0200

    add ban lookup command

---
+++ src/commands/mod/whyBanne.ts
@@ -0,0 +1,34 @@
+import { defineCommand } from "~/Command";
+import { codeblock, reply } from "~/util";
+import { resolveUser } from "~/util/resolvers";
+
+defineCommand({
+    name: "whybanne",
+    description: "Why Banne?",
+    aliases: ["wb", "whybanned", "banreason", "baninfo", "bi"],
+    guildOnly: true,
+    usage: "<user>",
+    async execute(msg, userResolvable) {
+        const user = await resolveUser(userResolvable).catch(() => null);
+        if (!user) {
+            return reply(msg, "who?");
+        }
+
+        const ban = await msg.guild.getBan(user.id).catch(() => null);
+        if (!ban) {
+            return reply(msg, "That user is not banned");
+        }
+
+        let reason = ban.reason || "No reason provided";
+        let actor = "idk who";
+
+        // ban command uses reason format like `actor: reason`
+        if (ban.reason?.split(" ")[0].endsWith(":")) {
+            const [a, ...rest] = ban.reason.split(" ");
+            reason = rest.join(" ");
+            actor = a;
+        }
+
+        reply(msg, `Banned by **${actor}**: ${codeblock(reason)}`);
+    },
+});
