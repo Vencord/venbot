ce75ef8c8bdecf51096704f4392190496ae3e76d
Vendicated <vendicated@riseup.net>
2024-09-07 01:03:51 +0200

    add forgejo-down command

---
+++ src/commands/info/forgejo-down.ts
@@ -0,0 +1,19 @@
+import { defineCommand } from "~/Commands";
+import { reply } from "~/util";
+
+defineCommand({
+    name: "forgejo-down?",
+    aliases: ["fj-down?", "ifd", "is-forgejo-down", "fjd?"],
+    description: "Check if Ninos Forgejo is down",
+    usage: null,
+    async execute(msg) {
+        const res = await fetch("https://git.nin0.dev/")
+            .then(r => r.ok
+                ? "No"
+                : `Yes (HTTP Status ${r.status})`
+            )
+            .catch(() => "Yes (Unreachable)");
+
+        return reply(msg, res ? "No" : "Yes");
+    }
+});
