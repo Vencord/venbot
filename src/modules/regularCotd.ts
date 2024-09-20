b32e1a75600562ae434336c92459f22ec94aab77
Vendicated <vendicated@riseup.net>
2024-09-20 03:22:28 +0200

    fix explosion

--- src/modules/regularCotd.ts
+++ src/modules/regularCotd.ts
@@ -15,21 +15,29 @@ interface ColorResponse {
     }
 }
 
-export async function drawBlobCatCozy(color: string, w = 256, h = 256) {
+export async function drawBlobCatCozy(color: string, size = 512) {
+    color = String(color)
+        .replaceAll("&", "&amp;")
+        .replaceAll("<", "&lt;")
+        .replaceAll(">", "&gt;")
+        .replaceAll("\"", "&quot;")
+        .replaceAll("'", "&#039;");
+
     const base = join(ASSET_DIR, "image-gen/regular-icon");
     const svgPath = join(base, "bcc.svg");
 
     const svgData = await readFile(svgPath, "utf-8");
-    const tintedSvg = svgData.replaceAll("#1a2b3c", JSON.stringify(color));
+    const tintedSvg = svgData.replaceAll("#1a2b3c", color);
+    console.log(tintedSvg);
     const svg = await loadImage(Buffer.from(tintedSvg));
 
-    svg.width = w;
-    svg.height = h;
+    svg.width = size;
+    svg.height = size;
 
-    const canvas = createCanvas(w, h);
+    const canvas = createCanvas(size, size);
 
     const ctx = canvas.getContext("2d");
-    ctx.drawImage(svg, 0, 0, w, h);
+    ctx.drawImage(svg, 0, 0, size, size);
 
     return canvas.toBuffer("image/png");
 }
