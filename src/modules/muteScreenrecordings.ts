552cafb1607433a1051ddf41732a8e66fdcbb7fe
Vendicated <vendicated@riseup.net>
2025-02-27 04:06:55 +0100

    remove obnoxious music from sadan screen recordings

---
+++ src/modules/muteScreenrecordings.ts
@@ -0,0 +1,55 @@
+import { mkdtemp, readFile, rm } from "fs/promises";
+import { tmpdir } from "os";
+import { join } from "path";
+
+import { Vaius } from "~/Client";
+import { execFile } from "~/util/childProcess";
+import { reply } from "~/util/discord";
+import { downloadToFile } from "~/util/fetch";
+
+const UsersToMute = ["521819891141967883"];
+const CategoryToMuteIn = "1216095839848501338";
+
+async function hasAudio(file: string) {
+    const res = await execFile("ffprobe", ["-i", file, "-show_streams", "-select_streams", "a", "-loglevel", "error"]);
+    return res.stdout.trim().length > 0;
+}
+
+async function muteVideo(file: string, outFile: string) {
+    const res = await execFile("ffmpeg", ["-i", file, "-c", "copy", "-an", outFile]);
+    return res.stdout.trim();
+}
+
+Vaius.on("messageCreate", async msg => {
+    if (!msg.inCachedGuildChannel()) return;
+    if (msg.channel.parentID !== CategoryToMuteIn) return;
+
+    const attachment = msg.attachments.first()!;
+    if (msg.attachments.size !== 1) return;
+    if (!["video/webm", "video/mp4"].includes(attachment?.contentType || "")) return;
+
+    if (!UsersToMute.includes(msg.author.id)) return;
+
+    const tempDir = await mkdtemp(join(tmpdir(), "vaius-mute-"));
+    try {
+        const file = join(tempDir, attachment.filename);
+        const mutedFile = join(tempDir, "muted-" + attachment.filename);
+
+        await downloadToFile(attachment.url, file);
+
+        if (!await hasAudio(file)) return;
+
+        await muteVideo(file, mutedFile);
+
+        await reply(msg, {
+            content: `From ${msg.author.mention} (video muted):\n\n${msg.content}`,
+            files: [{
+                contents: await readFile(mutedFile),
+                name: attachment.filename
+            }]
+        });
+        await msg.delete();
+    } finally {
+        await rm(tempDir, { recursive: true, force: true });
+    }
+});
