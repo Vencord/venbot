6a587f8909751e45070fcf35c179bf7b373fb80b
Vendicated <vendicated@riseup.net>
2025-04-17 20:44:53 +0200

    add gif [jumpscare|hammer] generate command

---
+++ src/commands/misc/gif.ts
@@ -0,0 +1,179 @@
+import { Canvas, createCanvas, Image, ImageData, loadImage } from "@napi-rs/canvas";
+import { readdirSync } from "fs";
+import { readFile } from "fs/promises";
+import { applyPalette, GIFEncoder, quantize } from "gifenc";
+import { decompressFrames, parseGIF } from "gifuct-js";
+import { User } from "oceanic.js";
+import { join } from "path";
+import { format } from "util";
+
+import { defineCommand } from "~/Commands";
+import { ASSET_DIR } from "~/constants";
+import { resolveUser } from "~/util/resolvers";
+
+type TemplateConfig = typeof import("../../../assets/image-gen/gif-templates/hammer/config.json");
+
+interface Template {
+    config: TemplateConfig;
+    frames: Image[];
+}
+
+const templateDir = join(ASSET_DIR, "image-gen/gif-templates");
+const templates = readdirSync(templateDir);
+
+templates.push("jumpscare");
+
+const templateCache = new Map<string, Template>();
+
+async function loadTemplate(name: string) {
+    let template = templateCache.get(name);
+    if (!template) {
+        const dir = join(templateDir, name);
+
+        const config: TemplateConfig = JSON.parse(await readFile(join(dir, "config.json"), "utf-8"));
+        const frames = await Promise.all(Array.from({ length: config.frames }, (_, i) => loadImage(join(dir, `frame-${i}.png`))));
+        template = { config, frames };
+        templateCache.set(name, template);
+    }
+
+    return template;
+}
+
+type AvatarFrameProvider = (index: number) => Image | Canvas;
+
+async function makeAvatarProvider(user: User): Promise<AvatarFrameProvider> {
+    const isGif = user.avatar?.startsWith("a_");
+    const avatarUrl = user.avatarURL(isGif ? "gif" : "png", 256);
+    const buf = await fetch(avatarUrl).then(res => res.arrayBuffer());
+
+    if (!isGif) {
+        const img = await loadImage(buf);
+        return (index: number) => img;
+    }
+
+    const gif = parseGIF(buf);
+    const frames = decompressFrames(gif, true);
+
+    const canvas = createCanvas(frames[0].dims.width, frames[0].dims.height);
+    const ctx = canvas.getContext("2d");
+    let imageData: ImageData;
+
+    return (index: number) => {
+        const frame = frames[index % frames.length];
+        const { width, height } = frame.dims;
+
+        if (!imageData || imageData.width !== width || imageData.height !== height) {
+            canvas.width = width;
+            canvas.height = height;
+            imageData = ctx.createImageData(width, height);
+        }
+
+        imageData.data.set(frame.patch);
+        ctx.putImageData(imageData, 0, 0);
+        return canvas;
+    };
+}
+
+defineCommand({
+    name: "gif",
+    description: "Insert a user's avatar into a gif template",
+    usage: `<template (one of: ${templates.join(" | ")})> [-reverse] <@user>`,
+
+    async execute({ reply, msg }, templateName, reverseFlagOrUserResolvable, userResolvable = msg.author.id) {
+        let reverse = ["-reverse", "--reverse", "-r"].includes(reverseFlagOrUserResolvable);
+        if (!reverse && reverseFlagOrUserResolvable) {
+            reverse = ["-reverse", "--reverse", "-r"].includes(userResolvable);
+            userResolvable = reverseFlagOrUserResolvable;
+        }
+
+        if (!templates.includes(templateName)) {
+            return reply(`Available templates: ${templates.join(", ")}`);
+        }
+
+        const user = await resolveUser(userResolvable);
+        if (!user) return reply("Unknown user");
+
+        const getAvatarFrame = await makeAvatarProvider(user);
+
+        if (templateName === "jumpscare") {
+            return reply({
+                files: [{
+                    name: `${user.username} jumpscare.gif`,
+                    contents: generateJumpscare(getAvatarFrame, reverse) as Buffer
+                }]
+            });
+        }
+
+        const { config: { width, height, avatarLocation, delay, filename }, frames } = await loadTemplate(templateName);
+
+        const gif = GIFEncoder();
+        const canvas = createCanvas(width, height);
+        const ctx = canvas.getContext("2d");
+
+        function processFrame(img: Image, idx: number) {
+            ctx.drawImage(img, 0, 0, width, height);
+            ctx.drawImage(getAvatarFrame(idx), avatarLocation.x, avatarLocation.y, avatarLocation.width, avatarLocation.height);
+
+            const { data } = ctx.getImageData(0, 0, width, height);
+
+            const palette = quantize(data, 256);
+
+            const index = applyPalette(data, palette);
+            gif.writeFrame(index, width, height, { palette, delay });
+        }
+
+        if (reverse) {
+            for (let i = frames.length - 1; i >= 0; i--) {
+                processFrame(frames[i], i);
+            }
+        } else {
+            for (let i = 0; i < frames.length; i++) {
+                processFrame(frames[i], i);
+            }
+        }
+
+        gif.finish();
+
+        return reply({ files: [{ name: format(filename, user.username) + ".gif", contents: gif.bytesView() as Buffer }] });
+    }
+});
+
+function generateJumpscare(getAvatarFrame: AvatarFrameProvider, reverse: boolean) {
+    const FINAL_SIZE = 400;
+    const START_SIZE = 10;
+    const GROWTH_FACTOR = 1.015;
+
+    const gif = GIFEncoder();
+    const canvas = createCanvas(FINAL_SIZE, FINAL_SIZE);
+    const ctx = canvas.getContext("2d");
+
+    const format = "rgb565";
+
+    function processFrame(size: number, i: number) {
+        const offset = (FINAL_SIZE - size) / 2;
+
+        ctx.drawImage(getAvatarFrame(i), offset, offset, size, size);
+        const { data } = ctx.getImageData(0, 0, FINAL_SIZE, FINAL_SIZE);
+
+        ctx.clearRect(0, 0, FINAL_SIZE, FINAL_SIZE);
+
+        const palette = quantize(data, 256, { format });
+
+        const index = applyPalette(data, palette, format);
+        gif.writeFrame(index, FINAL_SIZE, FINAL_SIZE, { palette, delay: 30, transparent: true });
+    }
+
+    if (reverse) {
+        for (let i = 0, size = FINAL_SIZE; size >= START_SIZE; i++, size **= 1 / GROWTH_FACTOR) {
+            processFrame(size, i);
+        }
+    } else {
+        for (let i = 0, size = START_SIZE; size <= FINAL_SIZE; i++, size **= GROWTH_FACTOR) {
+            processFrame(size, i);
+        }
+    }
+
+    gif.finish();
+
+    return gif.bytesView();
+}
