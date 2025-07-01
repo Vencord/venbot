70fb101fca136ab650ba0660442668b9a7557725
Vendicated <vendicated@riseup.net>
2025-07-01 23:54:42 +0200

    fix latinxia command

--- src/commands/misc/locateLatinx.ts
+++ src/commands/misc/locateLatinx.ts
@@ -1,5 +1,4 @@
 import { defineCommand } from "~/Commands";
-import { getMainGuild } from "~/util/discord";
 
 const LatinxRoleId = "1189709037688868964";
 
@@ -9,21 +8,6 @@ defineCommand({
     description: "Locate the elusive latinx",
     usage: null,
     async execute({ reply }) {
-        const guild = getMainGuild();
-
-        let candidates = guild!.members
-            .filter(m => m.roles.includes(LatinxRoleId));
-
-        if (!candidates.length) {
-            candidates = await guild!.fetchMembers().then(members =>
-                members.filter(m => m.roles.includes(LatinxRoleId))
-            );
-        }
-
-        const latinx = candidates
-            .filter(m => m.joinedAt)
-            .sort((a, b) => b.joinedAt!.getTime() - a.joinedAt!.getTime())[0];
-
-        reply(`<@${latinx.id}>`);
+        reply("Unfortunately latinxia was killed by the Brazilian government in 2023.... ☹️");
     }
 });
