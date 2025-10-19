52f03cb94c38cc9759d3efd75a4c4835fe707f1f
Vendicated <vendicated@riseup.net>
2025-10-19 19:22:25 +0200

    remove diacritics from nicknames

--- src/modules/moderate.ts
+++ src/modules/moderate.ts
@@ -142,6 +142,7 @@ export async function moderateNick(member: Member) {
     const normalizedName = name
         .normalize("NFKC")
         .replace(HoistCharactersRegex, "")
+        .replace(/[\u0300-\u036f\u0489]+/g, "") // diacritics
         .replaceAll(/[\u3099-\u309C]/g, "") // renders as a space and can be used for "empty" usernames
         .replaceAll("﷽", "")
         .trim()
