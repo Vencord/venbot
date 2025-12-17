07984c9b7115d2cebb8d5f003377236f3eb24ec3
Vendicated <vendicated@riseup.net>
2025-12-17 16:48:22 +0100

    Add emoji kitchen command

---
+++ src/commands/misc/cook.tsx
@@ -0,0 +1,63 @@
+import { defineCommand } from "~/Commands";
+import { Emoji } from "~/constants";
+import { fetchBuffer, fetchJson } from "~/util/fetch";
+import { ComponentMessage, Container, MediaGallery, MediaGalleryItem, TextDisplay } from "~components";
+
+const segmenter = new Intl.Segmenter("en", {
+    granularity: "grapheme"
+});
+
+function getEmojiSegments(input: string) {
+    return Array.from(segmenter.segment(input), s => s.segment);
+}
+
+async function cook(emoji1: string, emoji2: string) {
+    const url = "https://tenor.googleapis.com/v2/featured?" + new URLSearchParams({
+        key: "AIzaSyACvEq5cnT7AcHpDdj64SE3TJZRhW-iHuo",
+        client_key: "emoji_kitchen_funbox",
+        q: `${emoji1}_${emoji2}`,
+        collection: "emoji_kitchen_v6",
+        contentfilter: "high"
+    });
+
+    const data = await fetchJson(url).catch(() => null);
+    const result = data?.results?.[0];
+    if (!result) return null;
+
+    return {
+        url: result.url,
+        tags: result.tags
+    };
+}
+
+defineCommand({
+    name: "emoji-kitchen",
+    aliases: ["cook", "kitchen", "emojikitchen"],
+    usage: "<emoji1> <emoji2>",
+    description: "Cook up an emoji",
+    async execute({ react, reply }, emoji1, emoji2) {
+        if (!emoji2 && emoji1) {
+            [emoji1, emoji2] = getEmojiSegments(emoji1);
+        }
+        if (!emoji1 || !emoji2) return react(Emoji.QuestionMark);
+
+        const data = await cook(emoji1, emoji2);
+        if (!data) return reply("🧑‍🍳 No recipe found 🫨");
+
+        return reply(
+            <ComponentMessage
+                files={[{
+                    name: "image.png",
+                    contents: await fetchBuffer(data.url)
+                }]}
+            >
+                <TextDisplay># 🧑‍🍳</TextDisplay>
+                <Container>
+                    <MediaGallery>
+                        <MediaGalleryItem url={"attachment://image.png"} />
+                    </MediaGallery>
+                </Container>
+            </ComponentMessage>
+        );
+    },
+});
