11dd38085f3f8c1ce3992d0a9eac800cbdd4eb88
Vendicated <vendicated@riseup.net>
2026-08-09 02:12:25 +0200

    user emojis: handle size too large case

--- src/modules/userEmojis.ts
+++ src/modules/userEmojis.ts
@@ -1,4 +1,4 @@
-import { User } from "oceanic.js";
+import { DiscordRESTError, User } from "oceanic.js";
 import { Vaius } from "~/Client";
 import { db } from "~/db";
 import { fetchBuffer } from "~/util/fetch";
@@ -6,14 +6,27 @@ import { emojiCache, getEmojiData, uploadEmoji } from "./emojiManager";
 
 const getEmojiName = (user: User) => `av${user.id}`;
 
+async function uploadAvatarEmoji(user: User) {
+    for (let size = 128; size >= 16; size /= 2) {
+        try {
+            return await uploadEmoji(
+                getEmojiName(user),
+                await fetchBuffer(user.avatarURL(user.avatar!.startsWith("a_") ? "gif" : "png", size))
+            );
+        } catch (e) {
+            if (!(e instanceof DiscordRESTError) || e.code !== 50138 /* Failed to resize asset below the maximum size: 262144 */)
+                throw e;
+        }
+    }
+
+    throw new Error(`Failed to upload avatar emoji for ${user.id}: Avatar is too large to be added as an emoji`);
+}
+
 async function uploadUserEmoji(user: User) {
     const oldEmoji = emojiCache.get(getEmojiName(user));
     if (oldEmoji) await Vaius.application.deleteEmoji(oldEmoji.id);
 
-    const emoji = await uploadEmoji(
-        getEmojiName(user),
-        await fetchBuffer(user.avatarURL(user.avatar!.startsWith("a_") ? "gif" : "png", 128))
-    );
+    const emoji = await uploadAvatarEmoji(user);
 
     await db
         .insertInto("userAvatarEmojis")
