3eae7cdd09d49522ab522defc1e5f2d0251e2334
Vendicated <vendicated@riseup.net>
2026-08-29 20:22:49 +0200

    more fixes

--- src/modules/xp.ts
+++ src/modules/xp.ts
@@ -1,7 +1,9 @@
 import type { Message, User } from "oceanic.js";
 import { Vaius } from "~/Client";
 import Config from "~/config";
+import { Millis } from "~/constants";
 import { db } from "~/db";
+import { Deduper } from "~/util/Deduper";
 
 export function getXpForLevel(level: number): number {
     return Math.ceil((5 / 2) * (-1 + 20 * Math.pow(level, 2)));
@@ -33,9 +35,9 @@ export async function setXpForUser(user: User, xp: number): Promise<number> {
             userId: user.id,
             xp,
         })
-        .onConflict((oc) =>
+        .onConflict(oc =>
             oc.column("userId").doUpdateSet({
-                xp: (eb) => eb("xp.xp", "+", xp),
+                xp: eb => eb("xp.xp", "+", xp),
             }),
         )
         .returning("xp")
@@ -44,7 +46,7 @@ export async function setXpForUser(user: User, xp: number): Promise<number> {
     return result.xp;
 }
 
-const cooldowns: Record<string, number> = {};
+const cooldowns = new Deduper(1 * Millis.MINUTE);
 
 async function updateXpForMessage(msg: Message): Promise<number> {
     const gainedXp = getXpForMessage(msg);
@@ -56,21 +58,13 @@ async function updateXpForMessage(msg: Message): Promise<number> {
 Vaius.on("messageCreate", async msg => {
     if (!msg.inCachedGuildChannel()) return;
     if (msg.author.bot) return;
-    // ignore non eligible categories
     if (!msg.channel.parentID || !Config.xp.eligibleCategories.includes(msg.channel.parentID)) return;
-    // ignore messages with only images
     if (!msg.content) return;
-    // ignore venbot commands
-    if (Config.prefixes.some(prefix => msg.content.startsWith(prefix))) return;
-    // cooldown of 2.5 minutes
-    if (cooldowns[msg.author.id] > Date.now()) return;
-    cooldowns[msg.author.id] = Date.now() + (2.5 * 60000);
+    if (cooldowns.getOrAdd(msg.author.id)) return;
 
     const currentLevel = await updateXpForMessage(msg);
 
-    for (const [level, roleId] of Object.entries(Config.xp.rewards)
-        .sort(([a], [b]) => Number(a) - Number(b))) {
-
+    for (const [level, roleId] of Object.entries(Config.xp.rewards)) {
         const requiredLevel = Number(level);
 
         if (requiredLevel > currentLevel) break;
